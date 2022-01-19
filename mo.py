a8a761b830c2e7623239ff38b7c98da3a4ceaa3e
d8 <dixitpatel812@gmail.com>
2022-01-19 14:43:26 +0100

    cire function 1.0

--- mo.py
+++ mo.py
@@ -2,14 +2,19 @@ import mopy as mo
 from pypsa import Network
 
 
-def cire(data_folder_name, co2_limit):
+def cire(data_folder_name, start_limit=180, reduction=20, end_limit=0, m_factor=10e5, output_data=True):
     """
     :param data_folder_name: name of the folder where input files are stored
-    :param co2_limit: int value (in millions) of maximum allowable limit of total co2 emission in a time period. (Ex: in our case, total CO2 emission in a year 2030 (hourly resolved))
+    :param start_limit: int value (in millions) of maximum allowable limit of total co2 emission in a time period. (Ex: in our case, total CO2 emission in a year 2030 (hourly resolved))
+    :param reduction: Co2 emission reduction
+    :param end_limit: stop limit of reduction
+    :param m_factor: multiplication factor is 1000000
+    :param output_data: (bool) if true, gives output. otherwise, no output.
     :return:
+    note: producing output of  both (start and end) limits
     """
-    # co2 limit is in millions
-    co2_limit = co2_limit * int(10e6)
+    # start_limit as co2 limit at a point
+    co2_limit = start_limit
 
     # input folder
     input_folder_path = mo.path.join(mo.input_path, data_folder_name)
@@ -22,23 +27,36 @@ def cire(data_folder_name, co2_limit):
     if not mo.path.isdir(output_folder_path):
         mo.mkdir(output_folder_path)
 
-    # result folder
-    result_folder_path = mo.path.join(output_folder_path, str(co2_limit))
-    if not mo.path.isdir(result_folder_path):
-        mo.mkdir(result_folder_path)
     # create PyPSA network
     network = Network()
     network.import_from_csv_folder(input_folder_path)
 
-    while co2_limit >= 0:
+    while co2_limit >= end_limit:
+
         # add global constraint
         network.add("GlobalConstraint", "CO2_emission_limit", type="primary_energy", carrier_attribute="co2_emissions",
-                    constant=co2_limit, sense="<=")
+                    constant=co2_limit * int(m_factor), sense="<=")
         network.lopf(network.snapshots, solver_name="gurobi_direct")
-        network.export_to_csv_folder(result_folder_path)
+
+        # store output data?
+        if output_data:
+            # result folder
+            result_folder_path = mo.path.join(output_folder_path, str(co2_limit))
+            if not mo.path.isdir(result_folder_path):
+                mo.mkdir(result_folder_path)
+
+            network.export_to_csv_folder(result_folder_path)
+        else:
+            # if false then directly store the data that are important. (Ex: store, link and generators)
+            pass
+
+        # remove global constraint
         network.remove("GlobalConstraint", ["CO2_emission_limit"])
-        break
+
+        # loop condition variable
+        co2_limit = co2_limit - reduction
 
 
 if __name__ == "__main__":
-    cire("old_0.1.0", 18)
+    # cire("old_t_0", start_limit=180, reduction=20, end_limit=80)
+    # cire("old_t_0", start_limit=60, reduction=5, end_limit=0)
