776c047721a6071fb8c083910c0809b1387a5456
d8 <dixitpatel812@gmail.com>
2022-01-20 08:56:04 +0100

    opt updated

--- mo.py
+++ mo.py
@@ -6,21 +6,16 @@ heat_list = [['heat_boiler_oil', 'heat_boiler_gas'], ['heat_pump'], ["heat_stora
 hydrogen_list = [['steam_reforming'], ['electrolyser'], ['hydrogen_storage']]
 electricity_list = [['solar', 'wind_offshore', 'wind_onshore', 'biomass', 'hydropower', 'lignite_coal', 'hard_coal', 'natural_gas'], ['heat_pump', 'electrolyser'], ['hydro_storage', 'battery_storage']]
 
-opt_list = ['solar', 'wind_offshore', 'wind_onshore', 'heat_pump', 'electrolyser', 'hydro_storage', 'battery_storage', 'hydrogen_storage', "heat_storage"]
-opt_index = ['solar', 'wind_offshore', 'wind_onshore', 'biomass', 'hydropower', 'lignite_coal', 'hard_coal', 'natural_gas', 'oil', 'steam_reforming', 'heat_boiler_oil', 'heat_boiler_gas', 'support_gen_ele', 'support_gen_heat', 'support_gen_hydrogen', 'battery_charging', 'battery_discharging', 'hydro_charging', 'hydro_discharging', 'electrolyser', 'hydrogen_charging', 'hydrogen_discharging', 'heat_pump', 'heat_charging', 'heat_discharging', 'battery_storage', 'hydro_storage', 'hydrogen_storage', 'heat_storage']
 
-
-def opt(net, opt_file_list, opt_ind, opt_col_name):
+def opt(net, opt_col_name):
     """
     :param net: network
-    :param opt_file_list: list of necessary elements
-    :param opt_ind: pre-defined list
     :param opt_col_name: name of the column
     :return: data frame
     """
     o = pd.DataFrame()
     o = mo.ver(o, net.generators.loc[:, "p_nom_opt"], net.links.loc[:, "p_nom_opt"], net.stores.loc[:, "e_nom_opt"])
-    o = o.loc[opt_file_list, :]
+    # o = o.loc[opt_file_list, :]
     o.columns = [str(opt_col_name) + "Mt"]
     return o
 
@@ -71,7 +66,7 @@ def cire(data_folder_name, start_limit=180, reduction=20, end_limit=0, m_factor=
     co2_limit = start_limit
 
     # creating data frames
-    opt_file = pd.DataFrame(index=opt_list)
+    opt_file = pd.DataFrame()
 
     # input folder
     input_folder_path = mo.path.join(mo.input_path, data_folder_name)
@@ -121,7 +116,7 @@ def cire(data_folder_name, start_limit=180, reduction=20, end_limit=0, m_factor=
         electricity = mo.hor(energy(network, electricity_list, ele=True), -demand_df.loc[:, "electricity_demand"])
         electricity.to_csv(mo.path.join(result_files_folder_path, "electricity.csv"))
         # opt file
-        opt_file = mo.hor(opt_file, opt(network, opt_list, opt_index, co2_limit))
+        opt_file = mo.hor(opt_file, opt(network, co2_limit))
 
         # store output data?
         if output_data:
@@ -135,7 +130,7 @@ def cire(data_folder_name, start_limit=180, reduction=20, end_limit=0, m_factor=
 
         # loop condition variable
         co2_limit = co2_limit - reduction
-        break
+        # break
 
     # result files folder
     common_result_files_folder_path = mo.path.join(output_folder_path, "results")
@@ -148,4 +143,7 @@ def cire(data_folder_name, start_limit=180, reduction=20, end_limit=0, m_factor=
 
 if __name__ == "__main__":
     # cire("old_t_0", start_limit=180, reduction=20, end_limit=80)
-    cire("old_t_0", start_limit=20, reduction=5, end_limit=0)
+    # cire("old_t_0", start_limit=20, reduction=5, end_limit=0)
+    cire("fi_0.0", start_limit=180, reduction=20, end_limit=80)
+    cire("fi_0.0", start_limit=20, reduction=5, end_limit=0)
+
