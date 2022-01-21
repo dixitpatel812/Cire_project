b5d3bb957c26478369aef83c5cd9ca64845e94ba
d8 <dixitpatel812@gmail.com>
2022-01-21 08:22:31 +0100

    just stared again

--- mo_vis.py
+++ mo_vis.py
@@ -1,11 +1,24 @@
+import pandas as pd
 import mopy as mo
 
 
-def vis(results_folder_path):
-    output_folder_list = mo.listdir(mo.path.join(mo.output_path, mo.input_data_folder_name))
+def vis(result_path):
+
+    # output folder
+    output_folder_path = mo.path.join(mo.output_path, mo.input_data_folder_name)
+    if mo.path.isdir(output_folder_path):
+        # list of folder inside the output folder
+        output_folder_list = mo.listdir(output_folder_path)
+        output_folder_list.remove("results")
+
+        for co2_limit in output_folder_list:
+            result_folder_path = mo.path.join(output_folder_path, co2_limit, "results")
+
+    else:
+        print("Error!!! output folder path dose not exits")
+        return
 
 
 if __name__ == "__main__":
-    result_data_folder_path = mo.path.join("/run/media/d8/D8_HD/D/Sem_3/Cire/moosces/output_folder/fi_0.0/", "result")
+    result_data_folder_path = mo.path.join(mo.output_path, mo.input_data_folder_name, "results")
     vis(result_data_folder_path)
-    pass
