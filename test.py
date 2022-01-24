42144945a387dc5cffd3a77e1561952d354bc9a0
d8 <dixitpatel812@gmail.com>
2022-01-24 15:35:47 +0100

    added cost graph

--- test.py
+++ test.py
@@ -19,27 +19,43 @@ def vis_ind(data_frame, title, folder_path):
 def vis_new(data_folder_name):
     r_folder = mo.path.join(mo.output_path, data_folder_name, "results")
 
-    electricity = pd.read_csv(mo.path.join(r_folder, "electricity_total.csv"), index_col=0)
-    heat = pd.read_csv(mo.path.join(r_folder, "heat_total.csv"), index_col=0)
-    hydrogen = pd.read_csv(mo.path.join(r_folder, "hydrogen_total.csv"), index_col=0)
-    opt = pd.read_csv(mo.path.join(r_folder, "opt.csv"), index_col=0)
+    # electricity = pd.read_csv(mo.path.join(r_folder, "electricity_total.csv"), index_col=0)
+    # heat = pd.read_csv(mo.path.join(r_folder, "heat_total.csv"), index_col=0)
+    # hydrogen = pd.read_csv(mo.path.join(r_folder, "hydrogen_total.csv"), index_col=0)
+    # opt = pd.read_csv(mo.path.join(r_folder, "opt.csv"), index_col=0)
+    cost = pd.read_csv(mo.path.join(r_folder, "cost.csv"), index_col=0)
 
-    if not mo.path.isdir(mo.path.join(r_folder, "electricity_total_ind")):
-        mo.mkdir(mo.path.join(r_folder, "electricity_total_ind"))
-    vis_ind(electricity, "electricity_total", mo.path.join(r_folder, "electricity_total_ind"))
+    # if not mo.path.isdir(mo.path.join(r_folder, "electricity_total_ind")):
+    #     mo.mkdir(mo.path.join(r_folder, "electricity_total_ind"))
+    # vis_ind(abs(electricity), "electricity_total", mo.path.join(r_folder, "electricity_total_ind"))
+    #
+    # if not mo.path.isdir(mo.path.join(r_folder, "hydrogen_total_ind")):
+    #     mo.mkdir(mo.path.join(r_folder, "hydrogen_total_ind"))
+    # vis_ind(abs(hydrogen), "hydrogen_total", mo.path.join(r_folder, "hydrogen_total_ind"))
+    #
+    # if not mo.path.isdir(mo.path.join(r_folder, "heat_total_ind")):
+    #     mo.mkdir(mo.path.join(r_folder, "heat_total_ind"))
+    # vis_ind(abs(heat), "heat_total", mo.path.join(r_folder, "heat_total_ind"))
+    #
+    # if not mo.path.isdir(mo.path.join(r_folder, "opt_ind")):
+    #     mo.mkdir(mo.path.join(r_folder, "opt_ind"))
+    # vis_ind(opt, "opt", mo.path.join(r_folder, "opt_ind"))
 
-    if not mo.path.isdir(mo.path.join(r_folder, "hydrogen_total_ind")):
-        mo.mkdir(mo.path.join(r_folder, "hydrogen_total_ind"))
-    vis_ind(hydrogen, "hydrogen_total", mo.path.join(r_folder, "hydrogen_total_ind"))
+    plt.plot(cost.columns, cost.sum(axis=0)/1e9)
+    plt.xlabel("CO2_emission limit")
+    plt.ylabel("Total costs in billion Euro")
+    plt.title("Cost of reducing the CO2 emissions")
+    plt.savefig(mo.path.join(r_folder, "cost.png"))
+    plt.clf()
+    # plt.show
 
-    if not mo.path.isdir(mo.path.join(r_folder, "heat_total_ind")):
-        mo.mkdir(mo.path.join(r_folder, "heat_total_ind"))
-    vis_ind(heat, "heat_total", mo.path.join(r_folder, "heat_total_ind"))
-
-    if not mo.path.isdir(mo.path.join(r_folder, "opt_ind")):
-        mo.mkdir(mo.path.join(r_folder, "opt_ind"))
-    vis_ind(opt, "opt", mo.path.join(r_folder, "opt_ind"))
 
 
 if __name__ == "__main__":
-    vis_new("fi_2.0.1")
+    vis_new("fi_2.0")
+
+
+
+
+
+
