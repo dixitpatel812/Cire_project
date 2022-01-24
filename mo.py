426689c471168fcb7f52f3d7b69093246f146339
d8 <dixitpatel812@gmail.com>
2022-01-24 23:23:26 +0100

    name change

--- mo.py
+++ mo.py
@@ -202,4 +202,4 @@ def cire(data_folder_name, start_limit=180, reduction=20, end_limit=0, m_factor=
 
 
 if __name__ == "__main__":
-    cire(mo.input_data_folder_name)
+    cire(mo.input_data_folder_name, reduction=5)
