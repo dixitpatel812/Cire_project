6238f87bb3e58335d0a47f4526e3b7deeab5315b
d8 <dixitpatel812@gmail.com>
2022-01-27 14:12:36 +0100

    raised an error

--- mopy.py
+++ mopy.py
@@ -1,6 +1,7 @@
+import logging
 from os import path, mkdir, listdir
-import  pypsa
 from pandas import concat
+import logging
 
 
 def hor(*x):
@@ -23,17 +24,26 @@ def ver(*x):
     return data
 
 
+##############
+input_data_folder_name = "test"
+##############
+
 input_path = "/run/media/d8/D8_HD/D/Sem_3/Cire/moosces/input_folder/"
 output_path = "/run/media/d8/D8_HD/D/Sem_3/Cire/moosces/output_folder/"
 
-input_data_folder_name = "fi_3.0.1"
+# check input folder (D8)
+if not path.exists(path.join(input_path,input_data_folder_name)):
+    raise IOError('D8 : "%s" does not exist' % input_data_folder_name)
 
-# with fuel cell
+# # make output folder
+# # not needed here
+# if not path.exists(path.join(output_path, input_data_folder_name)):
+#     mkdir(path.join(output_path, input_data_folder_name))
+#     logging.info(" D8 : creating output folder %s @ %s" % (input_data_folder_name, path.join(output_path, input_data_folder_name)))
+
+
+# nested list with [[gen], [[out_link], [in_link]], [stores]]
+# improvement :  think or implement how you can make these lists from input files
 heat_list = [['heat_boiler_oil', 'heat_boiler_gas'], [["heat_charging"], ['heat_pump', "heat_discharging"]], ["heat_storage"]]
 hydrogen_list = [['steam_reforming'], [['hydrogen_charging', "hydrogen_fuel_cell"], ['electrolyser', "hydrogen_discharging"]], ['hydrogen_storage']]
 electricity_list = [['solar', 'wind_offshore', 'wind_onshore', 'biomass', 'hydropower', 'lignite_coal', 'hard_coal', 'natural_gas', "oil"], [['heat_pump', 'electrolyser', "battery_charging", "hydro_charging"], ["hydrogen_fuel_cell", "battery_discharging", "hydro_discharging"]], ['hydro_storage', 'battery_storage']]
-
-# without fuel cell
-# heat_list = [['heat_boiler_oil', 'heat_boiler_gas'], [["heat_charging"], ['heat_pump', "heat_discharging"]], ["heat_storage"]]
-# hydrogen_list = [['steam_reforming'], [['hydrogen_charging'], ['electrolyser', "hydrogen_discharging"]], ['hydrogen_storage']]
-# electricity_list = [['solar', 'wind_offshore', 'wind_onshore', 'biomass', 'hydropower', 'lignite_coal', 'hard_coal', 'natural_gas'], [['heat_pump', 'electrolyser', "battery_charging", "hydro_charging"], ["battery_discharging", "hydro_discharging"]], ['hydro_storage', 'battery_storage']]
