3e300b3fd0d87a01a5f710747ce5794a8aa743b0
d8 <dixitpatel812@gmail.com>
2022-01-31 09:31:28 +0100

    created updated_energy, energy_line_graph

--- vis.py
+++ vis.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import logging
 
+colour_list = ["black", "gray", "darkgrey", "silver", "red", "green", "blue", "sienna", "orchid", "fuchsia", "salmon", "tomato", "peru", "khaki", "plum", "purple", "violet", "pink", "yellow"]
+
 
 def cum_sum_file_total(df, e_list):
     df_new = pd.DataFrame(index=df.columns)
@@ -13,6 +15,39 @@ def cum_sum_file_total(df, e_list):
             df_new = mo.hor(df_new, df.loc[m, :])
 
     return df_new
+
+
+def vis_ind(data_frame, title, folder_path, ind=True):
+    """
+    :param data_frame: data frame with one row (axis=0) will be considered for plot at a time
+    :param title: main title
+    :param folder_path: out folder path
+    :param ind: True (individual graph will be saved) , False (all in one graph will be saved)
+    :return:
+    """
+    c = 0
+    if ind:
+        for i in data_frame.index:
+            plt.plot(data_frame.columns, data_frame.loc[i, :], colour_list[c], label=i)
+            plt.title(title + "  " + i)
+            plt.xlabel("CO2_emission limit")
+            plt.ylabel("")
+            plt.xticks(data_frame.columns, rotation="vertical")
+            plt.savefig(mo.path.join(folder_path, title + "_" + i + ".png"), dpi=400)
+            plt.legend(fontsize="xx-small")
+            plt.clf()
+            c = c + 1
+    if not ind:
+        for i in data_frame.index:
+            plt.plot(data_frame.columns, data_frame.loc[i, :], colour_list[c], label=i)
+            c = c + 1
+        plt.title(title)
+        plt.xlabel("CO2_emission limit")
+        plt.ylabel("")
+        plt.xticks(data_frame.columns, rotation="vertical")
+        plt.legend(fontsize="xx-small")
+        plt.savefig(mo.path.join(folder_path, title + ".png"), dpi=600)
+        plt.clf()
 
 
 def file_total(in_folder_path, energy_file_name, e_list_in, e_list_out, out_file_name, out_folder_path, ele=False):
@@ -51,7 +86,6 @@ def file_total(in_folder_path, energy_file_name, e_list_in, e_list_out, out_file
 
 
 # for stacked_energy_column_graph
-colour_list = ["black", "gray", "darkgrey", "silver", "red", "green", "blue", "sienna", "orchid", "fuchsia", "salmon", "tomato", "peru", "khaki", "plum", "purple", "violet", "pink", "yellow"]
 
 electricity_out = ['heat_pump', 'electrolyser', "", "electricity_demand"]
 electricity_in = ['biomass', 'hydropower', 'solar', 'wind_offshore', 'wind_onshore', 'lignite_coal', 'hard_coal', 'natural_gas', "oil", "hydrogen_fuel_cell"]
@@ -84,19 +118,53 @@ def stacked_energy_column_graph(data_folder_name):
     print("created electricity_total bar graph")
 
 
-def vis_ind(data_frame, title, folder_path):
-    for i in data_frame.index:
-        plt.plot(data_frame.columns, data_frame.loc[i, :], label=i)
-        plt.title(title + "  " + i)
-        plt.xlabel("CO2_emission limit")
-        plt.ylabel("")
-        plt.xticks(data_frame.columns, rotation="vertical")
-        plt.subplots_adjust(bottom=.17)
-        plt.savefig(mo.path.join(folder_path, title + "_" + i + ".png"))
-        plt.clf()
+def updated_energy(data_folder_name, csv_file_name, drooping=False, loss=False, demand=False):
+    """
+    :param demand: allow remove demand
+    :param loss: allow loss [charging + discharging]
+    :param drooping: allow removing [charging and discharging]
+    :param data_folder_name: input folder name
+    :param csv_file_name: fine name of the total energy file
+    :return: dataframe  with removed store
+    """
+
+    common_files_folder = mo.path.join(mo.output_path, data_folder_name, "common", "files")
+    energy = pd.read_csv(mo.path.join(common_files_folder, csv_file_name), index_col=0)
+
+    cd = pd.DataFrame(index=energy.columns)
+    # remove charging, discharging and store
+    for i in energy.index:
+        if demand:
+            if "demand" in i:
+                energy = energy.drop([i], axis=0)
+        if "storage" in i:
+            energy = energy.drop(i, axis=0)
+        if drooping:
+            if "discharging" in i:
+                cd = mo.hor(cd, energy.loc[i, :])
+                energy = energy.drop(i, axis=0)
+            elif "charging" in i:
+                cd = mo.hor(cd, energy.loc[i, :])
+                energy = energy.drop(i, axis=0)
+    if loss:
+        loss_s = cd.sum(axis=1)
+        loss_s.name = "losses"
+        energy = mo.hor(energy.T, loss_s)
+        energy = energy.T
+    return energy
 
 
-comparison_list = ['solar', 'wind_offshore', 'wind_onshore', 'electrolyser', 'heat_pump', 'biomass', 'natural_gas']
+def energy_line_graph(data_folder_name, csv_file_name):
+    common_folder = mo.path.join(mo.output_path, data_folder_name, "common")
+    line_graph_folder = mo.path.join(common_folder, "total_energy_line_graph")
+    # mo.folder_exist_err(common_folder, "total_energy_line_graph")
+    if not mo.path.exists(line_graph_folder):
+        mo.mkdir(line_graph_folder)
+    energy = updated_energy(data_folder_name, csv_file_name, drooping=True, demand=True, loss=True)
+    vis_ind(energy, csv_file_name[:-4], line_graph_folder, ind=False)
+
+
+comparison_list = ['hydropower']
 
 
 def vis_comparison_ind(data_folder_name, c_list):
@@ -181,4 +249,8 @@ def vis(data_folder_name, cost=False, curl=False, energy=False, opt=False):
 if __name__ == "__main__":
     # stacked_energy_column_graph("fi_4.0")
     # vis("fi_4.0", curl=True, cost=True)
-    vis_comparison_ind("fi_4.0", comparison_list)
+    # vis_comparison_ind("fi_4.0.1", comparison_list)
+    energy_line_graph("fi_4.0", "electricity_total.csv")
+    energy_line_graph("fi_4.0", "heat_total.csv")
+    energy_line_graph("fi_4.0", "hydrogen_total.csv")
+    # pass
